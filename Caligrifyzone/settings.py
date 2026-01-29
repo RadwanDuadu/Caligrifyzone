c2af36937ac9912caf6286f0eb9547a7a687249f
RadwanDuadu <mradwan444@gmail.com>
2026-01-29 06:06:27 +0000

    Add confirmation emails

--- Caligrifyzone/settings.py
+++ Caligrifyzone/settings.py
@@ -185,6 +185,7 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'caligrifyzone@example.com'
 
 COUNTRIES_FLAG_URL = None
 
