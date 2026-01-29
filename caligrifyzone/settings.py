6398d9cdce32d34f9755f9b2c21511dc7377f0d1
RadwanDuadu <mradwan444@gmail.com>
2026-01-29 21:14:33 +0000

    Update media folder name

--- caligrifyzone/settings.py
+++ caligrifyzone/settings.py
@@ -194,8 +194,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/staticfiles/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '/staticfiles/media')
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 100
