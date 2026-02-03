2eb2b613eeac2088d30cdabca81b80616b13ecc9
RadwanDuadu <mradwan444@gmail.com>
2026-02-03 03:43:51 +0000

    Update to remove 500 page error test

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('test-500/', views.trigger_500),
 ]
--- home/views.py
+++ home/views.py
@@ -6,7 +6,3 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
-
-
-def trigger_500(request):
-    raise Exception("Intentional test error for 500 page")
